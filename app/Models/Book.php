136b13f49c85910357325641a6bdc76a5fc8a44c
Le Quoc Cuong <lqcmailbox@gmail.com>
2022-06-21 16:58:39 +0700

    change: add Book's filter feature, add relationship, add $fillable

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,4 +11,49 @@ class Book extends Model
 
     public $timestamps = false;
     protected $table = 'book';
+
+    protected $fillable = [
+        'category_id',
+        'author_id',
+        'book_title',
+        'book_summary',
+        'book_price',
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author(){
+        return $this->belongsTo(Author::class);
+    }
+
+    public function review(){
+        return $this->hasMany(Review::class);
+    }
+
+    public function scopeFilter($query, array $filters) // Query Scope ( custom query builder method )
+    {
+//        dd(request('search'));
+        if (($filters['search'] ?? false)) { // isset operator in handy way.
+            $query
+                ->where('book_title', 'like', '%' . request('search') . '%')
+                ->orWhere('book_summary', 'like', '%' . request('search') . '%');
+        }
+
+        if (($filters['category'] ?? false)) {
+            $query
+                ->whereHas('category', fn ($query) => $query->where('category_id','=',request('category')));
+        }
+
+        if (($filters['author'] ?? false)) {
+            $query
+                ->whereHas('author', fn ($query) => $query->where('author_id','=',request('author')));
+        }
+
+        if (($filters['rating'] ?? false)) {
+            $query
+                ->whereHas('review', fn ($query) => $query->where('rating_start','=',request('rating')));
+        }
+    }
 }
